2754cdc411187c8f40265661794c1312bb722e78
TechWarrior12 <web.development@tritechnologies.net>
2026-09-10 15:40:54 +0500

    Add Microsoft Clarity tracking script for enhanced user insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -121,6 +121,17 @@ export default function RootLayout({
             gtag('config', '${GTAG_ID}');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="clarity-init" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yg2ydxw242");
+          `}
+        </Script>
       </body>
     </html>
   );
